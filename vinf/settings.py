70b93c9db5f5f872c5cdc9d69ec0b9dcc3d6141b
Kartik Anand <kartikanandwho@gmail.com>
2022-11-05 15:34:00 +0530

    .

--- vinf/settings.py
+++ vinf/settings.py
@@ -16,15 +16,24 @@ import django_heroku
 # import dj_database_url
 from decouple import config
 
+from .env import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2-%&74z^j#kg^@!ln8f23x_3i+&y57-wfc3@&(ezw_376us)7_'
+SECRET_KEY = os.environ['SECRET_KEY'] # Instead of your actual secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
